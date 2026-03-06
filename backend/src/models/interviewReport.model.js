67fe537fcdc696465cccf695a69c14978d18ed0e
Nikhil Randhawa <ns9013604867@gmail.com>
2026-03-06 12:57:12 +0530

    feat: add interview report model with schemas for technical and behavioral questions, skill gaps, and preparation plans

---
+++ backend/src/models/interviewReport.model.js
@@ -0,0 +1,90 @@
+const mongoose = require('mongoose')
+
+
+const technicalQuestionsSchema = new mongoose.Schema({
+    question: {
+        type: String,
+        required: [true, "Question is required"]
+    },
+    intention: {
+        type: String,
+        required: [true, "Intention is required"]
+    },
+    answer: {
+        type: String,
+        required: [true, "Answer is required"]
+    },
+}, { _id: false })
+
+const behavioralQuestionsSchema = new mongoose.Schema({
+    question: {
+        type: String,
+        required: [true, "Question is required"]
+    },
+    intention: {
+        type: String,
+        required: [true, "Intention is required"]
+    },
+    answer: {
+        type: String,
+        required: [true, "Answer is required"]
+    },
+
+}, { _id: false })
+
+const skillGapSchema = new mongoose.Schema({
+    skill: {
+        type: String,
+        required: [true, "Skill is required"]
+    },
+    severity: {
+        type: String,
+        enum: {
+            values: ["low", "medium", "high"],
+            message: "Severity must be low, medium or high"
+        },
+        required: [true, "Severity is required"]
+    }
+}, { _id: false })
+
+const preparationPlanSchema = new mongoose.Schema({
+    day: {
+        type: Number,
+        required: [true, "Day is required"]
+    },
+    focus: {
+        type: String,
+        required: [true, "Focus is required"]
+    },
+    tasks: [{
+        type: String,
+        required: [true, "Task is required"]
+    }]
+}, { _id: false })
+
+const interviewReportSchema = new mongoose.Schema({
+    jobDescription: {
+        type: String,
+        required: [true, "Job Description is required"]
+    },
+    resume: {
+        type: String
+    },
+    selfDescription: {
+        type: String
+    },
+    matchScore: {
+        type: Number,
+        min: 0,
+        max: 100
+    },
+    technicalQuestions: [technicalQuestionsSchema],
+    behavioralQuestions: [behavioralQuestionsSchema],
+    skillGaps: [skillGapSchema],
+    preparationPlan: [preparationPlanSchema],
+   
+},{timestamps:true})
+
+const InterviewReportModel = mongoose.model("InterviewReport", interviewReportSchema)
+
+module.exports = InterviewReportModel
